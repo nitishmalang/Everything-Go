8721c0750005b93cad836873f3042058956bcf01
Nitish Malang <71919457+nitishmalang@users.noreply.github.com>
2024-04-04 22:09:10 +0530

    Create main_1.go

---
+++ Fundamentals/main_1.go
@@ -0,0 +1,15 @@
+package main_1
+
+import (
+	"strings"
+)
+
+func ValidateUserInput(firstName string, lastName string, email string, userTickets uint, remainTickets uint) (bool, bool, bool) {
+
+	// input validation
+	isValidName := len(firstName) >= 2 && len(lastName) >= 2
+	isValidEmail := strings.Contains(email, "@") // gives back boolean result
+	isValidTicketNum := userTickets > 0 && userTickets < remainTickets
+
+	return isValidName, isValidEmail, isValidTicketNum
+}
